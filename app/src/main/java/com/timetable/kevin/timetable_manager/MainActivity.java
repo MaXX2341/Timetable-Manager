ae6c872145b5b8ff5f63c5405c88caf191d4c9f0
MaXX888 <36234094+MaXX888@users.noreply.github.com>
2018-02-21 15:14:15 +0100

    Kevin Test2

--- app/src/main/java/com/timetable/kevin/timetable_manager/MainActivity.java
+++ app/src/main/java/com/timetable/kevin/timetable_manager/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Hallo Max
         //Hallo Kevin!!
+        //Burnoutaradise
     }
 }
